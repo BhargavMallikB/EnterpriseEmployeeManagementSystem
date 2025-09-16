502db66282abb19a02c7f317b0a2c203110f885a
BhargavMallikB <bhargavmallikb@gmail.com>
2025-09-16 08:57:10 +0000

    Initialized empty component in UI-Library

---
+++ projects/eems-ui-library/src/lib/avatar/avatar.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'lib-avatar',
+  imports: [],
+  templateUrl: './avatar.component.html',
+  styleUrl: './avatar.component.css'
+})
+export class AvatarComponent {
+
+}
